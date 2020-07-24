a58be2d603751bfcb197eaa478e7afef5741f3a2
mushymane <59913310+mushymane@users.noreply.github.com>
2020-07-23 19:20:11 -0700

    adding project

---
+++ src/robots.js
@@ -0,0 +1,64 @@
+export const robots = [
+
+	{
+		id: 1,
+		name: 'Ahrim',
+		username: 'wiseguy',
+		email: 'yesbois1@gmail.com'
+	},
+	{
+		id: 2,
+		name: 'Dharok',
+		username: 'pewpew',
+		email: 'yesbois2@gmail.com'
+	},
+	{
+		id: 3,
+		name: 'Guthan',
+		username: 'yeetthat',
+		email: 'yesbois3@gmail.com'
+	},
+	{
+		id: 4,
+		name: 'Karil',
+		username: 'karen',
+		email: 'yesbois4@gmail.com'
+	},
+	{
+		id: 5,
+		name: 'Torag',
+		username: 'doublefistin',
+		email: 'yesbois5@gmail.com'
+	},
+	{
+		id: 6,
+		name: 'Verac',
+		username: 'shirtlessman',
+		email: 'yesbois6@gmail.com'
+	},
+	{
+		id: 7,
+		name: 'Jon Snow',
+		username: 'knowsnothing',
+		email: 'yesbois@gmail.com'
+	},
+	{
+		id: 8,
+		name: 'Daenerys Targaryen',
+		username: 'badbih',
+		email: 'yesbois8@gmail.com'
+	},
+	{
+		id: 9,
+		name: 'Jaime Lannister',
+		username: 'singlefistin',
+		email: 'yesbois9@gmail.com'
+	},
+	{
+		id: 10,
+		name: 'Arya Stark',
+		username: 'needles',
+		email: 'yesbois10@gmail.com'
+	},
+
+]
